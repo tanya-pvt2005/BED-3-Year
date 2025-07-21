a41fdf66fb61387c69c3e8ecb643d2d28e009e25
tanya-pvt2005 <tanya.pvt2005@gmail.com>
2025-07-21 15:02:18 +0530

    c2 Day8

--- Lec8/server.js
+++ Lec8/server.js
@@ -3,11 +3,40 @@ const app = express()
 const port = 4800
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+    //   res.send('Hello World!')
+    //   res.send("<h1>Hello Tanya</h1>")
+    //   res.sendFile(__dirname+"/index.html")
+    //   res.json({
+    //     "name":"Tanya",
+    //     "age" : 24
+    // })
+
+    res.end("Hi I am a girl")
+
+
+    //difference between res.send and res.end
+    // res.end only for string
+    //res.send: res header send hota hai
+
 })
 
+
+//Path Variable
+//if url has a variable : path variable; 2 ways to send it: query parameter and params
+// 1. Query Parameter
+// 2. Params
+
+//1. Query Parameter: query from client not made on server
+app.get('/watch',(req,res)=>{
+    let videoId = req.query.v
+    console.log(videoId) //req.query is an object; extract v from it
+    res.send("Got the id!")
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 
 })
 
