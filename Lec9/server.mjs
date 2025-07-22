beb79ed05107a99526f3317d11d6cc244d013b4e
tanya-pvt2005 <tanya.pvt2005@gmail.com>
2025-07-22 15:25:59 +0530

    c3

--- Lec9/server.mjs
+++ Lec9/server.mjs
@@ -3,22 +3,30 @@ import fs from 'fs'
 const app = express()
 
 app.use(express.json())
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-app.post("/signup",(req,res)=>{
+app.post("/signup", (req, res) => {
 
-    // const {name, age} = req.body
-    fs.appendFile('./user.txt', JSON.stringify(req.body),(err)=>{
+    const newUser = req.body;
 
-        if(err) console.log(err)
-        console.log(`Users successfully written in the file`)
-        console.log()
-    })
-    res.send("Done")
-})
+    fs.readFile("./users.txt", "utf-8", (err, data) => {
+        if (err) console.log(err);
 
-app.listen(3000,()=>{
+        let users = [];
+        if (data) users = JSON.parse(data);
+
+        users.push(newUser);
+
+        fs.writeFile("./users.txt", JSON.stringify(users), (err) => {
+            if (err) console.log(err);
+            console.log("Successfully written in file");
+        });
+    });
+});
+
+
+app.listen(3000, () => {
     console.log("Server is running at port 3000")
 })
