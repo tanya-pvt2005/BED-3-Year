aba1ca4704edb2e158d00a46447d0d931979d782
tanya-pvt2005 <tanya.pvt2005@gmail.com>
2025-07-30 14:56:45 +0530

    part2

--- Lec12/index.js
+++ Lec12/index.js
@@ -1,3 +1,6 @@
+//js for frontend
+
+//Part 1
 let btn = document.querySelector('#btn')
 let box = document.querySelector(".box");
 
@@ -6,4 +9,25 @@ let colors = ["blue", "green", "pink", "red", "purple", "gray", "yellow", "orang
 btn.onclick = () =>{
     let idx = Math.floor(Math.random()*10);
     box.style.backgroundColor = colors[idx];
+}
+
+//Part 2
+let generate = document.querySelector('#generate')
+let  colorBox= document.querySelector("#color-box");
+let stop = document.querySelector("#stop");
+function randomColor(){
+    let idx = Math.floor(Math.random()*10);;
+    return colors[idx];
+}
+generate.onclick = () =>{
+
+    intervalId = setInterval(()=>{
+        let color = randomColor();
+        colorBox.style.backgroundColor = color;
+    }, 500)
+}
+stop.onclick = () => {
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
 }
